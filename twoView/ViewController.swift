1a037f3830713a704de622cbb25f35ab24ca8fc8
calculator <ddd>
2022-01-08 20:45:26 +0600

    Initial Commit

---
+++ twoView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  twoView
+//
+//  Created by Икрамджан Абдукадыров on 8/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
